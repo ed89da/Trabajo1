5e3005b3c07dc7aa4d4596bdacc1465048fa2f00
Dalma <dalmafolio18@gmail.com>
2024-08-23 10:34:59 -0300

    CLASE PARA PRODUCTOS COMESTIBLES

---
+++ src/Comestible.java
@@ -0,0 +1,16 @@
+public abstract class Comestible extends Producto {
+    private String fechaVencimiento;
+    private int calorias;
+    private boolean importado;
+
+    public Comestible(String id, String descripcion, int cantidadStock, double precioUnidad, double porcentajeGanancia, boolean disponibleVenta, String fechaVencimiento, int calorias, boolean importado) {
+        super(id, descripcion, cantidadStock, precioUnidad, porcentajeGanancia, disponibleVenta);
+        this.fechaVencimiento = fechaVencimiento;
+        this.calorias = calorias;
+        this.importado = importado;
+    }
+
+    public boolean isImportado() {
+        return importado;
+    }
+}
